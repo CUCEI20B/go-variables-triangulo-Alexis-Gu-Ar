d7681f82054dff50459186a493abf942925f33ba
Alexis Guzmán <alegu@Alexiss-MacBook-Pro.local>
2020-09-28 07:54:17 -0500

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var height uint16
+	var width uint16
+	fmt.Scan(&height)
+	fmt.Scan(&width)
+	area := height * width / 2.0
+	fmt.Print(area)
 }
